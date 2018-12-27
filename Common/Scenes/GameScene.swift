239e4e9ac9e3ae3935633814739c9f090926862a
Eduardo Irías <eduardoirias@me.com>
2018-12-27 17:39:11 +0100

    Add tvOS Target

--- Common/Scenes/GameScene.swift
+++ Common/Scenes/GameScene.swift
@@ -52,10 +52,12 @@ class GameScene: SKScene {
     
     var currentChips = [Chip]()
     
-    
     var currentDialogIndex = 0
     
+    var isScrollingInput = false
     private var lastUpdateTime : TimeInterval = 0
+    
+    // MARK: - Scene
     
     override func sceneDidLoad() {
         
@@ -82,6 +84,29 @@ class GameScene: SKScene {
         renderDialog()
     }
     
+    override func didMove(to view: SKView) {
+        super.didMove(to: view)
+        
+        if isScrollingInput {
+            let swipeRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(GameScene.swipe))
+            swipeRecognizer.direction = .right
+            self.view!.addGestureRecognizer(swipeRecognizer)
+            
+            let leftSwipeRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(GameScene.swipe))
+            leftSwipeRecognizer.direction = .left
+            self.view!.addGestureRecognizer(leftSwipeRecognizer)
+            
+            let downSwipeRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(GameScene.swipe))
+            downSwipeRecognizer.direction = .down
+            self.view!.addGestureRecognizer(downSwipeRecognizer)
+            
+            let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(GameScene.tap(_:)))
+            self.view!.addGestureRecognizer(tapRecognizer)
+        }
+    }
+    
+    // MARK: - Logic
+    
     func resetBoard() {
         currentChips.removeAll()
         board.reset()
@@ -105,6 +130,30 @@ class GameScene: SKScene {
         removeSelectedChips()
         return isWinner()
     }
+    
+    func addToCurrentSelected(coin: Chip) {
+        if enemy.state != .waiting {
+            enemy.wait()
+        }
+        
+        if currentChips.contains(coin) {
+            coin.isSelected = false
+            currentChips.removeAll(where: { $0 == coin})
+            return
+        }
+        
+        if coin.boxIndex != currentChips.first?.boxIndex {
+            for currentChip in currentChips {
+                currentChip.isSelected = false
+            }
+            currentChips.removeAll()
+        }
+        
+        coin.isSelected = true
+        currentChips.append(coin)
+    }
+    
+    // MARK: - Touch Events
     
     func touchDown(atPoint pos : CGPoint) {
         if let continueButton = self.nodes(at: pos).first(where: { $0.name == "continueButton"}) {
@@ -130,6 +179,8 @@ class GameScene: SKScene {
             }
             return
         }
+        
+        if isScrollingInput { return }
         
         continueButton.alpha = 1.0
         resetButton.alpha = 1.0
@@ -171,30 +222,96 @@ class GameScene: SKScene {
         if state != .playing { return }
         
         if let chip = self.nodes(at: pos).first(where: { $0.name == "coin"}) as? Chip {
-            
-            if enemy.state != .waiting {
-                enemy.wait()
-            }
-            
-            if currentChips.contains(chip) {
-                chip.isSelected = false
-                currentChips.removeAll(where: { $0 == chip})
-                return
-            }
-            
-            if chip.boxIndex != currentChips.first?.boxIndex {
-                for currentChip in currentChips {
-                    currentChip.isSelected = false
-                }
-                currentChips.removeAll()
-            }
-            
-            chip.isSelected = true
-            currentChips.append(chip)
+            self.addToCurrentSelected(coin: chip)
         }
     }
     
+    // MARk: Swie
+    @objc func tap(_ sender: Any?) {
+        if isBottonNavigationSelected {
+            
+            self.state = .dialog
+            
+            if self.removeSelectedChipsAndEvaluateWinner() {
+                self.dialogNode.state = .crying
+                self.enemy.state = .crying
+            } else {
+                
+                var chipCount = 0
+                for box in board.boxes {
+                    for _ in box.chips {
+                        chipCount += 1
+                    }
+                }
+                if chipCount == 0 {
+                    self.dialogNode.state = .celebrating
+                    self.enemy.state = .celebrating
+                } else {
+                    self.dialogNode.state = .waiting
+                }
+            }
+            self.currentDialogIndex = Int.random(in: 0..<self.dialogNode.state.texts.count)
+            self.renderDialog()
+            
+        } else {
+            if let currentTVSelectedChip = currentTVSelectedChip {
+                self.addToCurrentSelected(coin: currentTVSelectedChip)
+            }
+        }
+    }
+    
+    @objc func swipe(_ sender: Any?) {
+        if state != .playing { return }
+        
+        guard let gesture = sender as? UISwipeGestureRecognizer else { return }
+        
+        currentTVSelectedChip?.setScale(1.0)
+        
+        if isBottonNavigationSelected && (gesture.direction == .right || gesture.direction == .left) {
+            continueButton.setScale(1.0)
+        }
+        
+        if gesture.direction == .down {
+            isBottonNavigationSelected = true
+            continueButton.setScale(1.4)
+            return
+        }
+        
+        if gesture.direction == .right {
+            currentChipIndex += 1
+        }else {
+            currentChipIndex -= 1
+        }
+        
+        if currentChipIndex == board.boxes[currentBoxIndex].chips.count {
+            currentBoxIndex += 1
+            currentChipIndex = 0
+        } else if currentChipIndex == -1 {
+            currentBoxIndex -= 1
+            if currentBoxIndex >= 0 {
+                currentChipIndex = board.boxes[currentBoxIndex].chips.count - 1
+            }
+        }
+        
+        if currentBoxIndex == board.boxes.count {
+            currentBoxIndex = 0
+            currentChipIndex = 0
+        } else if currentBoxIndex == -1 {
+            currentBoxIndex = board.boxes.count - 1
+            currentChipIndex = board.boxes[currentBoxIndex].chips.count - 1
+        }
+        if board.boxes[currentBoxIndex].chips.count >= currentChipIndex {
+            currentTVSelectedChip = board.boxes[currentBoxIndex].chips[currentChipIndex]
+        }
+        currentTVSelectedChip?.setScale(1.4)
+
+    }
+    
+    // MARK: - Targets
+    
+    // MARK: iOS
     #if os(iOS)
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
@@ -207,15 +324,57 @@ class GameScene: SKScene {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
+    // MARK: tvOS
+    #elseif os(tvOS)
     
+    var currentBoxIndex = 0
+    var currentChipIndex = 0
+    var isBottonNavigationSelected = false
+    var currentTVSelectedChip: Chip?
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+//
+//        let w = (self.size.width + self.size.height) * 0.05
+//        let spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+//
+//        spinnyNode.lineWidth = 2.5
+//
+//        spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
+//        spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
+//                                          SKAction.fadeOut(withDuration: 0.5),
+//                                          SKAction.removeFromParent()]))
+//
+//        spinnyNode.position = touches.first!.location(in: self)
+//        spinnyNode.zPosition = 100
+//        self.addChild(spinnyNode)
+    }
+    
+    // MARK: macOS
     #else
+    
     override func mouseDown(with event: NSEvent) {
         self.touchDown(atPoint: event.location(in: self))
     }
     override func mouseUp(with event: NSEvent) {
         self.touchUp(atPoint: event.location(in: self))
     }
+    
     #endif
+    
+    // MARK: - AI
     
     func aiPlay() {
         
@@ -262,6 +421,9 @@ class GameScene: SKScene {
             
         }
     }
+    
+    // MARK: - Update
+    
     func renderDialog() {
         dialogNode?.text = dialogNode.state.texts[currentDialogIndex]
 
---
+++ Noodles Challenge tvOS/GameViewController.swift
@@ -0,0 +1,47 @@
+//
+//  GameViewController.swift
+//  Noodles Challenge tvOS
+//
+//  Created by Eduardo Irias on 12/26/18.
+//  Copyright © 2018 Estamp. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
+        // including entities and graphs.
+        if let scene = GKScene(fileNamed: "GameScene") {
+            
+            // Get the SKScene from the loaded GKScene
+            if let sceneNode = scene.rootNode as! GameScene? {
+                
+                sceneNode.isScrollingInput = true
+                
+                // Copy gameplay related content over to the scene
+                sceneNode.entities = scene.entities
+                sceneNode.graphs = scene.graphs
+                
+                // Set the scale mode to scale to fit the window
+                sceneNode.scaleMode = .aspectFill
+                
+                // Present the scene
+                if let view = self.view as! SKView? {
+                    view.presentScene(sceneNode)
+                    
+                    view.ignoresSiblingOrder = true
+                    
+                    view.showsFPS = true
+                    view.showsNodeCount = true
+                }
+            }
+        }
+    }
+
+}
