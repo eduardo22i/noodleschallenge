ac45db9ba0d6ab28f4b3014d4f0560524f69f8d3
Eduardo Irías <eduardoirias@me.com>
2023-08-14 18:56:24 +0200

    Add New Target

---
+++ Noodles Challenge VR/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Noodles Challenge VR
+//
+//  Created by Eduardo Irias on 8/14/23.
+//  Copyright © 2023 Estamp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Noodles Challenge VR/Noodles_Challenge_VRApp.swift
@@ -0,0 +1,18 @@
+//
+//  Noodles_Challenge_VRApp.swift
+//  Noodles Challenge VR
+//
+//  Created by Eduardo Irias on 8/14/23.
+//  Copyright © 2023 Estamp. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct Noodles_Challenge_VRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
